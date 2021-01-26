2beee6bd1bd2d56a50e1a4e146f16fc3759755fa
cleiton-isaias <cleitonisaias@live.com>
2021-01-26 10:04:16 -0300

    Correção propriedade da imagem

--- src/components/Heads/index.js
+++ src/components/Heads/index.js
@@ -6,22 +6,22 @@ export default function Heads() {
 <Head>
               {/* <!-- Primary Meta Tags --> */}
         <title>Zero 1 Quiz</title>
-        <meta name="title" content="Zero 1 Quiz"/>
-        <meta name="description" content="Quiz para Testar seus conhecimentos sobre Zelda!"/>
+        <meta name="Zero 1 Quiz" content="Zero 1 Quiz"/>
+        <meta name="Zero 1 Quiz" content="Quiz para Testar seus conhecimentos sobre Zelda!"/>
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website"/>
         <meta property="og:url" content="https://metatags.io/"/>
         <meta property="og:title" content="The legend of Zelda"/>
         <meta property="og:description" content="Quiz para Testar seus conhecimentos sobre Zelda!"/>
-        <meta property="og:image" content="../imagens/link.png"/>
+        <meta property="../imagens/link.png" content="../imagens/link.png"/>
 
       {/*   <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="twitter:url" content="https://metatags.io/"/>
         <meta property="twitter:title" content="The legend of Zelda"/>
         <meta property="twitter:description" content="Quiz para Testar seus conhecimentos sobre Zelda!"/>
-        <meta property="twitter:image" content="../imagens/link.png"/>
+        <meta property="../imagens/link.png" content="../imagens/link.png"/>
       </Head> 
     );
 }   
