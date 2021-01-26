21053ae838269310573e41fbcd183d9130328333
cleiton-isaias <cleitonisaias@live.com>
2021-01-26 09:25:45 -0300

    Correção endereço da imagem

--- src/components/Heads/index.js
+++ src/components/Heads/index.js
@@ -14,14 +14,14 @@ export default function Heads() {
         <meta property="og:url" content="https://metatags.io/"/>
         <meta property="og:title" content="The legend of Zelda"/>
         <meta property="og:description" content="Quiz para Testar seus conhecimentos sobre Zelda!"/>
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+        <meta property="og:image" content="../imagens/link.png"/>
 
       {/*   <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="twitter:url" content="https://metatags.io/"/>
         <meta property="twitter:title" content="The legend of Zelda"/>
         <meta property="twitter:description" content="Quiz para Testar seus conhecimentos sobre Zelda!"/>
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+        <meta property="twitter:image" content="../imagens/link.png"/>
       </Head> 
     );
 }   
