696b704b7b00429068ce941502b741af079e30ed
cleiton-isaias <cleitonisaias@live.com>
2021-01-26 14:12:13 -0300

    Insrindo url da imagem 2

--- src/components/Heads/index.js
+++ src/components/Heads/index.js
@@ -11,17 +11,17 @@ export default function Heads() {
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website"/>
-        <meta property="https://zero1quiz-base-git-master.cleiton-isaias.vercel.app/" content="https://metatags.io/"/>
+        <meta property="https://zero1quiz-base-git-master.cleiton-isaias.vercel.app/" content={"../imagens/link.png"}/>
         <meta property="og:title" content="The legend of Zelda"/>
         <meta property="og:description" content="Quiz para Testar seus conhecimentos sobre Zelda!"/>
         <meta property="og:image" content={"../imagens/link.png"}/>
 
         {/*   <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="https://zero1quiz-base-git-master.cleiton-isaias.vercel.app/" content="https://metatags.io/"/>
+        <meta property="https://zero1quiz-base-git-master.cleiton-isaias.vercel.app/" content={"../imagens/link.png"}/>
         <meta property="twitter:title" content="The legend of Zelda"/>
         <meta property="twitter:description" content="Quiz para Testar seus conhecimentos sobre Zelda!"/>
-        <meta property="twitter:image" content="../imagens/link.png"/>
+        <meta property="twitter:image" content={"../imagens/link.png"}/>
 </Head> 
     );
 }   
